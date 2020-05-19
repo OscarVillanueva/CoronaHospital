7fde68f9807015c3c1f59095a15489f58eddb054
Oscar Villanueva <javiondo68@gmail.com>
2020-05-18 22:32:16 -0500

    fetch the services in reports

--- components/Layout/DoctorList.jsx
+++ components/Layout/DoctorList.jsx
@@ -3,6 +3,7 @@ import { css } from "@emotion/core";
 import { useSelector } from "react-redux";
 import { Grid4, Container } from "../includes/Grid";
 import axios from '../../config/axios';
+import firebase from '../../firebase/firebase';
 import DoctorService from './DoctorService';
 
 const DoctorList = () => {
@@ -13,7 +14,23 @@ const DoctorList = () => {
     useEffect(() => {
 
         const fetchDoctors = async () => {
-            const { data } = await axios.get(`/services?owner.id_ne=${current.id}`)
+
+            const response = await firebase.db.collection("services").get()
+
+            const data = []
+
+            response.forEach(doc => {
+
+                // Solo guardamos los que son diferentes
+                if ( doc.data().owner.id !== current.id )
+                    data.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
+
+            });
+
+            // const { data } = await axios.get(`/services?owner.id_ne=${current.id}`)
             setServices(data)
         }
 
